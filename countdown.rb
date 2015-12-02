092452f2673f2e7285c2ef1d40f3aab199f5d1b3
vanessaa0 <vanessa.castro.simontech.2016@gmail.com>
2015-12-02 03:21:43 +0000

    Finished this countdown lab

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(new_year)
+  while new_year > 0 do
+    puts "#{new_year} SECOND(S)!"
+    new_year -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+puts "HAPPY NEW YEAR!"
+
+def countdown_with_sleep(number)
+  while number > 0 do
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1)
+  end
+  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
